4dd54817d4cfcdc3d82999f5e40353bcf2ccdf6d
jhlobel <jhlobel@gmail.com>
2024-01-30 18:41:34 -0800

    Add files via upload

---
+++ fig4_ms1/dms_scoring.R
@@ -0,0 +1,119 @@
+library(stats4)
+library(stringr)
+
+#format counts
+format_counts = function(files){
+  table_ls <- lapply(files, function(x){
+    f = read.table(x, header=T,sep = '')
+    colnames(f) = c(x,'Name')
+    #delete the unassign read (row 1)
+    f = f[2:length(f[,1]),]
+    return(f)
+  })
+  counts = Reduce(function(...) merge(..., all = TRUE, by = "Name"), table_ls)
+  counts[is.na(counts)] <- 0
+  return(counts)
+}
+
+#load rep1 and rep2 YFP files and iRFP files
+setwd("~/yeast-idr-analysis-main/raw_counts/dms_scan_sortSeq/mutscan_rep1/")
+dms_yfp_R1 = format_counts(list.files(pattern ='.txt')[!grepl('iRFP',list.files(pattern='.txt'))])
+colnames(dms_yfp_R1) = c('Name','US-1','US-2','FL','FR','L','R')
+dms_irfp_R1 = format_counts(list.files(pattern ='.txt')[!grepl('YFP',list.files(pattern='.txt'))])
+colnames(dms_irfp_R1) = c('Name','FL','FR','L','R','US-1','US-2')
+
+setwd("~/yeast-idr-analysis-main/raw_counts/dms_scan_sortSeq/mutscan_rep2/")
+dms_yfp_R2 = format_counts(list.files(pattern ='.txt')[!grepl('iRFP',list.files(pattern='.txt'))])
+colnames(dms_yfp_R2) = c('Name','US','FL','FR','L','R')
+dms_irfp_R2 = format_counts(list.files(pattern ='.txt')[!grepl('YFP',list.files(pattern='.txt'))])
+colnames(dms_irfp_R2) = c('Name','FL','FR','L','R','US-1','US-2')
+
+#cutoff peps that have less than 25 reads in unsorted, or less than 5 reads in sorted
+dms_yfp_R1 = dms_yfp_R1[dms_yfp_R1$`US-1` >= 25 & dms_yfp_R1$`US-2` >= 25 & rowSums(dms_yfp_R1[,4:7]) >=5,]
+dms_yfp_R2 = dms_yfp_R2[dms_yfp_R2$US >= 25 & rowSums(dms_yfp_R2[,3:6]) >=5,] 
+dms_irfp_R1 = dms_irfp_R1[dms_irfp_R1$`US-1` >= 25 & dms_irfp_R1$`US-2` >= 25 & rowSums(dms_irfp_R1[,2:5]) >=5,]
+dms_irfp_R2 = dms_irfp_R2[dms_irfp_R2$`US-1` >= 25 & dms_irfp_R2$`US-2` >= 25 & rowSums(dms_irfp_R2[,2:5]) >=5,]
+
+#prepare pseudocounts for each replicate
+pseudo_yfp_r1 <- data.frame(FL = dms_yfp_R1$FL / sum(dms_yfp_R1$FL),
+                             L = dms_yfp_R1$L / sum(dms_yfp_R1$L),
+                             R = dms_yfp_R1$R / sum(dms_yfp_R1$R),
+                             FR = dms_yfp_R1$FR/ sum(dms_yfp_R1$FR))
+
+pseudo_yfp_r2 <- data.frame(FL = dms_yfp_R2$FL / sum(dms_yfp_R2$FL),
+                             L = dms_yfp_R2$L / sum(dms_yfp_R2$L),
+                             R = dms_yfp_R2$R / sum(dms_yfp_R2$R),
+                             FR = dms_yfp_R2$FR/ sum(dms_yfp_R2$FR))
+
+pseudo_irfp_r1 <- data.frame(FL = dms_irfp_R1$FL / sum(dms_irfp_R1$FL),
+                            L = dms_irfp_R1$L / sum(dms_irfp_R1$L),
+                            R = dms_irfp_R1$R / sum(dms_irfp_R1$R),
+                            FR = dms_irfp_R1$FR/ sum(dms_irfp_R1$FR))
+
+pseudo_irfp_r2 <- data.frame(FL = dms_irfp_R2$FL / sum(dms_irfp_R2$FL),
+                            L = dms_irfp_R2$L / sum(dms_irfp_R2$L),
+                            R = dms_irfp_R2$R / sum(dms_irfp_R2$R),
+                            FR = dms_irfp_R2$FR/ sum(dms_irfp_R2$FR))
+
+# bounds are c(LOWER, UPPER). #bins correspond to flow gates (Left to Right)
+loglikeBin <- function(bounds, peak) {
+  log(pnorm(bounds[[2]], mean=peak) - pnorm(bounds[[1]], mean=peak))
+}
+
+binBounds = list(FL=qnorm(c(0.001, 0.24999)),
+                 L=qnorm(c(0.25,0.49999)),
+                 R=qnorm(c(0.50,0.749999)),
+                 FR=qnorm(c(0.75,0.99999)))
+
+mlePeak <- function(pseudos, bounds) {
+  peak0 <- 0
+  
+  nll <- function(peak) {
+    ll <- c(sum(pseudos$FL) * loglikeBin(bounds$FL, peak),
+            sum(pseudos$L)  * loglikeBin(bounds$L,  peak),
+            sum(pseudos$R)  * loglikeBin(bounds$R,  peak),
+            sum(pseudos$FR) * loglikeBin(bounds$FR, peak))
+    ll[!is.finite(ll)] <- -36
+    res <- -sum(1e8*ll, na.rm=TRUE)
+    res
+  }
+  
+  mle(minuslogl = nll,
+      start=list(peak=peak0),
+      method="L-BFGS-B",
+      lower=-3, upper=3,
+      control=(REPORT=1))
+}
+
+#run yfp and iRFP for both rep 1 and rep 2
+activity_score_r1 <- lapply(seq(1,nrow(dms_yfp_R1)), function(i) { mlePeak(dms_yfp_R1[i,], binBounds)})
+dms_yfp_R1$activity_score_r1 <- sapply(activity_score_r1,coef) 
+
+activity_score_r2 <- lapply(seq(1,nrow(dms_yfp_R2)), function(i) { mlePeak(dms_yfp_R2[i,], binBounds)})
+dms_yfp_R2$activity_score_r2 <- sapply(activity_score_r2,coef)
+
+stability_score_r1 <- lapply(seq(1,nrow(dms_irfp_R1)), function(i) { mlePeak(dms_irfp_R1[i,], binBounds)})
+dms_irfp_R1$stability_score_r1 <- sapply(stability_score_r1,coef) 
+
+stability_score_r2 <- lapply(seq(1,nrow(dms_irfp_R2)), function(i) { mlePeak(dms_irfp_R2[i,], binBounds)})
+dms_irfp_R2$stability_score_r2 <- sapply(stability_score_r2,coef) 
+
+#make counts and score data and save in processed reads
+colnames(dms_yfp_R1)[2:7] = paste0(colnames(dms_yfp_R1)[2:7],'_YFP_','R1')
+colnames(dms_yfp_R2)[2:6] = paste0(colnames(dms_yfp_R2)[2:6],'_YFP_','R2')
+colnames(dms_irfp_R1)[2:7] = paste0(colnames(dms_irfp_R1)[2:7],'_irfp_','R1')
+colnames(dms_irfp_R2)[2:7] = paste0(colnames(dms_irfp_R2)[2:7],'_irfp_','R2')
+all_counts = merge(merge(merge(dms_yfp_R1, dms_yfp_R2, by='Name'), dms_irfp_R1, by='Name'), dms_irfp_R2, by='Name')
+scores = all_counts[,c('Name','activity_score_r1','activity_score_r2','stability_score_r1','stability_score_r2')]
+scores$avg_activity = rowMeans(scores[,c("activity_score_r1",'activity_score_r2')])
+scores$activity_sd = apply(scores[,c("activity_score_r1",'activity_score_r2')],1,sd)
+scores$avg_stability = rowMeans(scores[,c("stability_score_r1",'stability_score_r2')])
+scores$stability_sd = apply(scores[,c("activity_score_r1",'activity_score_r2')],1,sd)
+
+setwd("~/yeast-idr-analysis-main/processed_scores/")
+write.csv(all_counts,"dms_counts.csv")
+write.csv(scores, 'dms_scores.csv')
+
+
+
+
---
+++ fig4_ms1/motif_finder.R
@@ -0,0 +1,147 @@
+library(depmixS4)
+
+
+setwd("~/yeast-idr-analysis-main/processed_scores/")
+scores = read.csv("dms_scores.csv")
+hmm_acts = read.csv("scram_vs_hmm.csv") #if this doesn't exist, run script in fig6 (scramble_assignment.R)
+
+setwd("~/yeast-idr-analysis-main/screening_lib_seqs/")
+aa_seq = read.csv("mutational_library_translation.csv")
+
+#get motifs as ones that are sig diff in scramble and have negative scram vs hmm scores
+motif = hmm_acts[hmm_acts$scram_diff >= 0 & hmm_acts$z_adj <= 0.05,] #excludes peps with more acitivty when scrambled, small num
+composition = hmm_acts[hmm_acts$z_adj > 0.05, ]
+
+format_data <-function(df=scores,pep, stability_cutoff = -1){
+  foo = df[grepl(paste0(pep,'_Mut'), df$Name),]
+  foo$start = sapply(seq(1,nrow(foo)),function(x){as.numeric(strsplit(foo$Name[x],'_')[[1]][5])}) 
+  foo$cutoff_activity = ifelse(foo$avg_stability <= stability_cutoff, NA,foo$avg_activity) #replace activity scores of unstable with NA
+  #if   missing peptides, fill in with Na
+  missing = which(!(seq(1,46) %in% foo$start)) 
+  empty = data.frame(matrix(ncol=ncol(foo),nrow=length(missing)))
+  colnames(empty) = colnames(foo)
+  empty$start = missing
+  foo = rbind(foo,empty)
+  return(foo[order(foo$start),])
+} 
+
+fit_hmm_with_aa <- function(df=scores, pep, aas=aa_seq, stability_cutoff = -1,states=2){
+  print(pep)
+  temp = format_data(df=df, pep=pep, stability_cutoff = stability_cutoff) #format data
+  hmm = fit(depmix(temp$avg_activity~1, data=temp, nstates = states)) #fit to hmm, gets state values
+  post = posterior(hmm, type='viterbi')[1] #get which position is in each state
+  temp$state = post$state
+  s1 = summary(hmm, which='all')[1] 
+  s2 = summary(hmm, which='all')[2]
+  temp$hmm_act = sapply(temp$state, function(x){ifelse(x == 1, s1,s2)}) #match activity to state
+  temp$end = temp$start + 4 #for downstream plotting
+  temp$aa = strsplit(aas[aas$name == paste0(pep,'_WT_0'),'seq'],'')[[1]][1:nrow(temp)]
+  return(temp[complete.cases(temp),]) 
+}
+
+#build df with transition numbers and state len
+get_HMM_params <- function(df=scores, pep_ls, aas = aa_seq, stability_cutoff = -1, states=2){
+  params = data.frame(pep = rep(0,length(pep_ls)),
+                      num_transitions = rep(0,length(pep_ls)),
+                      min_s1_len = rep(0,length(pep_ls)),
+                      min_s2_len = rep(0,length(pep_ls)))
+  
+  for(i in 1:length(pep_ls)){
+    hmm = fit_hmm_with_aa(df=df, pep=pep_ls[i], aas=aa_seq, stability_cutoff = stability_cutoff,states = states)
+    state_lens = with(rle(hmm$state),data.frame(values,lengths))
+    params[i,] = list((pep_ls[i]), 
+                sum(diff(hmm$state) != 0), #get number of transitions
+                min(state_lens$lengths[state_lens$values == 1]), #get shortest state 1 
+                min(state_lens$lengths[state_lens$values == 2])) #get shortest state 2
+    
+  }
+  return(params)
+}
+
+motif_params = get_HMM_params(pep_ls=motif$name)
+motif_params$size_crit = ifelse(motif_params$min_s1_len >= 2 & motif_params$min_s2_len >= 2, TRUE,FALSE )
+motif_pass = motif_params[motif_params$num_transitions <= 5 & motif_params$size_crit,]
+
+
+#this  divides the HMM fits into active/inactive regions, aware that the first residue of a functional motif
+#starts at the last residue of the first inactivating mutation. 
+find_motif_V4 <- function(df=scores, pep_ls,aas = aa_seq, stability_cutoff = -1,states=2,minlen=4){
+  master = data.frame('pep' = NA, 'activity' = NA, 'seq' = NA, 'state'=NA)
+  for(pep in pep_ls){
+    #get the aa_sequence and initialize start at 1 
+    pepseq = strsplit(aas[grepl(paste0(pep,'_WT_0'),aas$name),'seq'],'')[[1]]
+    pos = 1
+
+    #fit hmm, divide into regions within same state
+    hmm = fit_hmm_with_aa(df=df, pep=pep, aas=aa_seq, stability_cutoff = stability_cutoff,states = states)
+    #divide the hmm into list of separate regions of continuous activity. 
+    hmm$region = c(0, cumsum(abs(diff(hmm$state)) > 0))
+    regions = split(hmm, hmm$region)
+    #here, multiple if loops evaluate different scenarios. 
+    for(i in 1:length(regions)){
+      #1: only evaluate if regions is longer that min specified
+      if(nrow(regions[[i]]) > minlen){
+        #for inert mutation (i.e is in the more repressive activity), collect all from start to stop +4. If its a deleterious, collect from start +4
+        if(regions[[i]]$hmm_act[1] == min(hmm$hmm_act) & i != length(regions)){
+          subseq = paste0(pepseq[pos:(regions[[i]]$start[nrow(regions[[i]])]+4)],collapse = '')
+          master[nrow(master)+1,] = list(paste0(pep,'_',pos),
+                                                min(hmm$hmm_act),
+                                                subseq,'inactive')
+          #reset the new starting position for the next motif, which must be a deleterious mutation
+          pos = (regions[[i]]$start[nrow(regions[[i]])] + 5)
+        }      
+        #check if region consists of disruptive mutations. For deleterious mutations, begin counting from the 5th position
+        #the last residue mutated of the first window is responsible for activity. 
+        if(regions[[i]]$hmm_act[1] == max(hmm$hmm_act) & i != length(regions)){
+          subseq = paste0(pepseq[pos:regions[[i]]$start[nrow(regions[[i]])]], collapse = '')
+          master[nrow(master)+1,] = list(paste0(pep,'_',pos),
+                                      max(hmm$hmm_act),
+                                      subseq, 'active')
+          pos = (regions[[i]]$start[nrow(regions[[i]])]+1)
+
+        }
+        #Assumes all residues in the last region, from x to 50, are in the same state
+        if(i == length(regions)){
+          subseq = paste0(pepseq[pos:50],collapse = '') #hardcode the 50, as all pepseqs are 50aa
+          master[nrow(master)+1,] = list(paste0(pep,'_',pos),
+                                      regions[[i]]$hmm_act[1],
+                                      subseq,
+                                      ifelse(regions[[i]]$hmm_act[1] == max(hmm$hmm_act),'active','inactive'))
+        }
+      }
+    }
+  }
+  return(master[2:nrow(master),])
+}
+  
+#active state means it has higher activity score (in this case, activity when mutated).Converse for inactive
+submotifs = find_motif_V4(pep_ls = motif_pass$pep)
+submotifs$len = nchar(submotifs$seq)
+
+
+#qc = data.frame(pep = unique(sub("^(([^_]*_){2}[^_]*)_.*$", "\\1",submotifs$pep)), 
+ #               assembled = rep(0,length(unique(sub("^(([^_]*_){2}[^_]*)_.*$", "\\1",submotifs$pep)))))
+#qc$assembled = sapply(qc$pep,function(x){paste0(submotifs[grepl(x,submotifs$pep),'seq'],collapse = '')})
+#qc$len = nchar(qc$assembled) #check that there aren't any super long submotifs, looks good
+
+
+
+
+setwd("~/yeast-idr-analysis-main/fig4_ms1/")
+
+write.csv(submotifs,'subMotifs_hmm.csv',row.names = F)
+write.csv(motif, 'motifs_full.csv',row.names = F) #also write the full motif data
+
+#get the composition seq
+
+compo_output = data.frame(pep=composition$name,
+                          activity = composition$act_state,
+                          seq = aa_seq[aa_seq$name %in% paste0(composition$name,'_WT_0'),'seq'],
+                          state = rep('active',nrow(composition)),
+                          len = rep(50,nrow(composition)))
+
+if(!(file.exists('~/yeast-idr-analysis-main/fig6_ms1/'))){
+  write.csv(compo_output,'~/yeast-idr-analysis-main/composition.csv',row.names = F)
+}
+
+
